d8a506300203cd4b3dcd41bd2751c3af063ad35e
Daniel Siekawitch <daniel.siekawitch@gmail.com>
2019-10-02 16:08:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,12 @@ class Dog
   attr_accessor :name, :breed, :id
   # attr_reader 
   
-  def initialize(id = nil, name:, breed:)
+  def initialize(attributes)
     # @id = id 
     # @name = name 
     # @breed = breed 
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
   end
   
   
